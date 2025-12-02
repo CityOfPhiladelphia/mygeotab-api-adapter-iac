aaec7c18d12ff59db2f926fbaf12fced8238f3b5
Ryan Weast <ryweast@gmail.com>
2025-12-02 12:15:47 -0500

    Migrate Terraform State Storage to S3 (#13)
    * Use s3 terraform state
    * Remove comment

--- terraform/common/main.tf
+++ terraform/common/main.tf
@@ -7,12 +7,13 @@ terraform {
       version = "~> 6.0"
     }
   }
-  cloud {
-    organization = "Philadelphia"
 
-    workspaces {
-      name = "mygeotab-api-adapter-common"
-    }
+  backend "s3" {
+    bucket = "phl-citygeo-terraform-state"
+    # CHANGE ME!
+    key          = "mygeotab-api-adapter/common"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
 
